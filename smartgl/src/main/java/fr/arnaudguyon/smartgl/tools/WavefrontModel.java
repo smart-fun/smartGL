9a3303232b64310032fcdeb6d3db41bde45eedd0
Arnaud Guyon <nonuguyon@gmail.com>
2016-11-25 22:06:12 +0100

    WavefrontModel loader now handles Vertex Colors, but only supports triangles. Fixed an issue when merging strips (some triangles were inverted, so bug visible only when disabling double sided faces).

--- smartgl/src/main/java/fr/arnaudguyon/smartgl/tools/WavefrontModel.java
+++ smartgl/src/main/java/fr/arnaudguyon/smartgl/tools/WavefrontModel.java
@@ -25,6 +25,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import fr.arnaudguyon.smartgl.opengl.ColorList;
 import fr.arnaudguyon.smartgl.opengl.Face3D;
 import fr.arnaudguyon.smartgl.opengl.Object3D;
 import fr.arnaudguyon.smartgl.opengl.Texture;
@@ -108,10 +109,20 @@ public class WavefrontModel {
         float mY;
         float mZ;
 
+        float mR, mG, mB;
+        boolean mHasColors = false;
+
         Vertex(float x, float y, float z) {
             mX = x;
             mY = y;
             mZ = z;
+        }
+
+        void setColors(float r, float g, float b) {
+            mHasColors = true;
+            mR = r;
+            mG = g;
+            mB = b;
         }
     }
 
@@ -166,6 +177,19 @@ public class WavefrontModel {
                             if ((x != null) && (y != null) && (z != null)) {
                                 Vertex vertex = new Vertex(x, y, z);
                                 mVertex.add(vertex);
+
+                                if (elements.length > 6) {  // get vertex colors
+                                    Float r = fromString(elements[4]);
+                                    Float g = fromString(elements[5]);
+                                    Float b = fromString(elements[6]);
+                                    if (    (r != null) && (g != null) && (b != null) &&
+                                            (r >= 0) && (r <= 1) && (g >= 0) && (g <=1) && (b >= 0) && (b <= 1)) {
+                                        vertex.setColors(r, g, b);
+                                        break;
+                                    } else {
+                                        throw new RuntimeException("Vertex Colors error line " + lineNumber);
+                                    }
+                                }
                                 break;
                             }
                         }
@@ -199,67 +223,10 @@ public class WavefrontModel {
                         }
                         break;
                     case "f":   // FACES
-                        if (elements.length == 5) { // Quad
-                            Integer vA = intPart(elements, 1, 0);   // Vertex A
-                            Integer tA = intPart(elements, 1, 1);   // Texture UV A
-                            Integer nA = intPart(elements, 1 ,2);   // Normal A
-                            IndexInfo indexA = IndexInfo.create(vA, tA, nA);
-
-                            Integer vB = intPart(elements, 2, 0);   // Vertex B
-                            Integer tB = intPart(elements, 2, 1);   // Texture UV B
-                            Integer nB = intPart(elements, 2 ,2);   // Normal B
-                            IndexInfo indexB = IndexInfo.create(vB, tB, nB);
-
-                            Integer vC = intPart(elements, 3, 0);   // Vertex C
-                            Integer tC = intPart(elements, 3, 1);   // Texture UV C
-                            Integer nC = intPart(elements, 3 ,2);   // Normal C
-                            IndexInfo indexC = IndexInfo.create(vC, tC, nC);
-
-                            Integer vD = intPart(elements, 4, 0);   // Vertex D
-                            Integer tD = intPart(elements, 4, 1);   // Texture UV D
-                            Integer nD = intPart(elements, 4 ,2);   // Normal D
-                            IndexInfo indexD = IndexInfo.create(vD, tD, nD);
-
-                            if ((indexA != null) && (indexB != null) && (indexC != null) && (indexD != null)) {
-                                Strip strip = new Strip(stripTextureName);
-                                strip.addIndex(indexA);
-                                strip.addIndex(indexB);
-                                strip.addIndex(indexD); // quad converted to triangle strip
-                                strip.addIndex(indexC);
-                                mStrips.add(strip);
-                            } else {
-                                throw new RuntimeException("Quad error line " + lineNumber);
-                            }
-
-                        } else if (elements.length == 4) {    // Triangle
-                            Integer vA = intPart(elements, 1, 0);   // Vertex A
-                            Integer tA = intPart(elements, 1, 1);   // Texture UV A
-                            Integer nA = intPart(elements, 1 ,2);   // Normal A
-                            IndexInfo indexA = IndexInfo.create(vA, tA, nA);
-
-                            Integer vB = intPart(elements, 2, 0);   // Vertex B
-                            Integer tB = intPart(elements, 2, 1);   // Texture UV B
-                            Integer nB = intPart(elements, 2 ,2);   // Normal B
-                            IndexInfo indexB = IndexInfo.create(vB, tB, nB);
-
-                            Integer vC = intPart(elements, 3, 0);   // Vertex C
-                            Integer tC = intPart(elements, 3, 1);   // Texture UV C
-                            Integer nC = intPart(elements, 3 ,2);   // Normal C
-                            IndexInfo indexC = IndexInfo.create(vC, tC, nC);
-
-                            if ((indexA != null) && (indexB != null) && (indexC != null)) {
-                                Strip strip = new Strip(stripTextureName);
-                                strip.addIndex(indexA);
-                                strip.addIndex(indexB);
-                                strip.addIndex(indexC);
-                                mStrips.add(strip);
-                            } else {
-                                throw new RuntimeException("Triangle error line " + lineNumber);
-                            }
-
+                        if (elements.length == 4) {    // Triangle
+                            addFaceStrips(lineNumber, stripTextureName, elements, 1, 3);
                         } else {
-                            // TODO: load polygonal faces (more than 4 vertex). I'm not sure how to split into triangles
-                            throw new RuntimeException("Face Index error line " + lineNumber);
+                            throw new RuntimeException("Only triangles supported, error line " + lineNumber);
                         }
                         break;
                 }
@@ -268,6 +235,34 @@ public class WavefrontModel {
             e.printStackTrace();
         }
         Log.i("DONE", "DONE");
+    }
+
+    private void addFaceStrips(int lineNumber, String materialName, String[] elements, int offsetStart, int numberOfVertex) {
+
+        // TODO: check order, seem to be dependant on file loaded :/
+        // Maybe suggest to use a tool to re-export (http://meshlab.sourceforge.net/ ?)
+        int[] triangleIndex = {0, 2 , 1};
+//        int[] quadIndex = {0, 2, 1, 3};
+//        // TODO: convex polygon should be like 0,1,2 0,2,3 0,3,4 0,4,5 ...
+//        int[] order = (numberOfVertex == 3) ? triangleIndex : quadIndex;
+
+        ArrayList<IndexInfo> indexInfos = new ArrayList<>(numberOfVertex);
+        for(int i=0; i<numberOfVertex; ++i) {
+            int index = triangleIndex[i];
+            Integer vA = intPart(elements, offsetStart + index, 0);   // Vertex A
+            Integer tA = intPart(elements, offsetStart + index, 1);   // Texture UV A
+            Integer nA = intPart(elements, offsetStart + index ,2);   // Normal A
+            IndexInfo indexA = IndexInfo.create(vA, tA, nA);
+            if (indexA == null) {
+                throw new RuntimeException("Face error line " + lineNumber);
+            }
+            indexInfos.add(indexA);
+        }
+        Strip strip = new Strip(materialName);
+        for(IndexInfo indexInfo : indexInfos) {
+            strip.addIndex(indexInfo);
+        }
+        mStrips.add(strip);
     }
 
     private Float fromString(String string) {
@@ -295,74 +290,6 @@ public class WavefrontModel {
         return null;
     }
 
-//    private void removeDuplicates() {
-//
-//        int nbDuplicateVertex = 0;
-//        int nbDuplicateUV = 0;
-//        int nbDuplicateNormals = 0;
-//
-//        SparseIntArray duplicateVertex = new SparseIntArray(mVertex.size());    // duplicated, original
-//        SparseIntArray duplicateUV = new SparseIntArray(mUVs.size());
-//        SparseIntArray duplicateNormal = new SparseIntArray(mNormals.size());
-//
-//        // VERTEX
-//        {
-//            for (int index = 0; index < mVertex.size() - 1; ++index) {
-//                Vertex vertex = mVertex.get(index);
-//                for (int iOther = index + 1; iOther < mVertex.size(); ++iOther) {
-//                    Vertex other = mVertex.get(iOther);
-//                    if (vertex.equals(other)) {
-//                        ++nbDuplicateVertex;
-//                        duplicateVertex.put(iOther, index);
-//                    }
-//                }
-//            }
-//        }
-//
-//        // MAPPING
-//        {
-//            for (int index = 0; index < mUVs.size() - 1; ++index) {
-//                UV uv = mUVs.get(index);
-//                for (int iOther = index + 1; iOther < mUVs.size(); ++iOther) {
-//                    UV other = mUVs.get(iOther);
-//                    if (uv.equals(other)) {
-//                        ++nbDuplicateUV;
-//                        duplicateUV.put(iOther, index);
-//                    }
-//                }
-//            }
-//        }
-//
-//        // NORMALS
-//        {
-//            for (int index = 0; index < mNormals.size() - 1; ++index) {
-//                Normal normal = mNormals.get(index);
-//                for (int iOther = index + 1; iOther < mNormals.size(); ++iOther) {
-//                    Normal other = mNormals.get(iOther);
-//                    if (normal.equals(other)) {
-//                        ++nbDuplicateNormals;
-//                        duplicateNormal.put(iOther, index);
-//                    }
-//                }
-//            }
-//        }
-//
-//        Log.i(TAG, nbDuplicateVertex + " duplicated Vertex found");
-//        Log.i(TAG, nbDuplicateUV + " duplicated UVs found");
-//        Log.i(TAG, nbDuplicateNormals + " duplicated Normals found");
-//
-//        // Fix them!
-//        for(Strip strip : mStrips) {
-//            ArrayList<IndexInfo> indexes = strip.mIndexes;
-//            for(IndexInfo indexInfo : indexes) {
-//                indexInfo.mVertexIndex = duplicateVertex.get(indexInfo.mVertexIndex, indexInfo.mVertexIndex);
-//                indexInfo.mUVIndex = duplicateUV.get(indexInfo.mUVIndex, indexInfo.mUVIndex);
-//                indexInfo.mNormalIndex = duplicateNormal.get(indexInfo.mNormalIndex, indexInfo.mNormalIndex);
-//            }
-//        }
-//
-//    }
-
     // Groups Strips of same material into 1 big strip (1 triangle strip per face)
     private void mergeStrips() {
         for(int iStrip = 0; iStrip<mStrips.size() - 1; ++iStrip) {
@@ -374,6 +301,7 @@ public class WavefrontModel {
 
                     IndexInfo originLastIndex = origin.mIndexes.get(origin.mIndexes.size() - 1);
                     IndexInfo otherFirstIndex = other.mIndexes.get(0);
+                    origin.addIndex(originLastIndex);
                     origin.addIndex(originLastIndex);
                     origin.addIndex(otherFirstIndex);
                     origin.addAll(other.mIndexes);
@@ -391,34 +319,54 @@ public class WavefrontModel {
      */
     public Object3D toObject3D() {
 
+        final boolean hasUV = (mUVs.size() > 0);
+
         Object3D object3D = new Object3D();
         for(Strip strip : mStrips) {
+
             Face3D face3D = new Face3D();
             int nbIndex = strip.mIndexes.size();
+
             VertexList vertexList = new VertexList();
             vertexList.init(nbIndex);
-            UVList uvList = new UVList();
-            uvList.init(nbIndex);
+
+            UVList uvList = null;
+            ColorList colorList = null;
+            if (hasUV) {
+                uvList = new UVList();
+                uvList.init(nbIndex);
+            } else {
+                colorList = new ColorList();
+                colorList.init(nbIndex);
+            }
+
             for(IndexInfo indexInfo : strip.mIndexes) {
                 int vertexIndex = indexInfo.mVertexIndex;
                 Vertex vertex = mVertex.get(vertexIndex);
                 vertexList.add(vertex.mX, vertex.mY, vertex.mZ);
 
-                if (mUVs.size() > 0) {
+                if (hasUV) {
                     int uvIndex = indexInfo.mUVIndex;
                     UV uv = mUVs.get(uvIndex);
                     uvList.add(uv.mU, uv.mV);
+                } else if (vertex.mHasColors) {
+                    colorList.add(vertex.mR, vertex.mG, vertex.mB, 1);  // TODO: find a way to change the alpha channel?
                 } else {
-                    uvList.add((float) Math.random(), (float) Math.random());
+                    throw new RuntimeException("Model must have texture UVs or vertex Colors");
                 }
             }
             vertexList.finalizeBuffer();
             face3D.setVertexList(vertexList);
-            uvList.finalizeBuffer();
-            face3D.setUVList(uvList);
 
-            Texture texture = mTextures.get(strip.mTextureName);
-            face3D.setTexture(texture);
+            if (hasUV) {
+                uvList.finalizeBuffer();
+                face3D.setUVList(uvList);
+                Texture texture = mTextures.get(strip.mTextureName);
+                face3D.setTexture(texture);
+            } else {
+                colorList.finalizeBuffer();
+                face3D.setColorList(colorList);
+            }
 
             object3D.addFace(face3D);
         }
