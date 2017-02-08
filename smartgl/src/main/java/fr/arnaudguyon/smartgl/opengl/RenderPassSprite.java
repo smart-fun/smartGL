6e82cf30a440b8f675a5ce82466bd8dbbe198d1a
Arnaud Guyon <arnaud@castbygenii.com>
2017-02-08 22:56:16 +0100

    Improved memory allocation for Sprite sorting

--- smartgl/src/main/java/fr/arnaudguyon/smartgl/opengl/RenderPassSprite.java
+++ smartgl/src/main/java/fr/arnaudguyon/smartgl/opengl/RenderPassSprite.java
@@ -38,30 +38,33 @@ public class RenderPassSprite extends RenderPass {
     @Override
     void sortObjects() {
         Vector<RenderObject> objects = getRenderObjects();
-        Collections.sort(objects, new Comparator<RenderObject>() {
-            @Override
-            public int compare(RenderObject leftO, RenderObject rightO) {
-                if (leftO instanceof Sprite) {
-                    Sprite leftSprite = (Sprite) leftO;
-                    if (rightO instanceof Sprite) {
-                        Sprite rightSprite = (Sprite) rightO;
-                        int diff = rightSprite.getDisplayPriority() - leftSprite.getDisplayPriority();
-                        if (diff == 0) {
-                            return 0;
-                        } else if (diff > 0) {
-                            return 1;
-                        } else {
-                            return -1;
-                        }
+        Collections.sort(objects, SPRITE_COMPARATOR);
+    }
+
+    private static final Comparator<RenderObject> SPRITE_COMPARATOR = new Comparator<RenderObject>() {
+        @Override
+        public int compare(RenderObject leftO, RenderObject rightO) {
+            if (leftO instanceof Sprite) {
+                Sprite leftSprite = (Sprite) leftO;
+                if (rightO instanceof Sprite) {
+                    Sprite rightSprite = (Sprite) rightO;
+                    int diff = rightSprite.getDisplayPriority() - leftSprite.getDisplayPriority();
+                    if (diff == 0) {
+                        return 0;
+                    } else if (diff > 0) {
+                        return 1;
                     } else {
                         return -1;
                     }
-                } else if (rightO instanceof Sprite) {
-                    return 1;
                 } else {
-                    return 0;
+                    return -1;
                 }
+            } else if (rightO instanceof Sprite) {
+                return 1;
+            } else {
+                return 0;
             }
-        });
-    }
+        }
+    };
+
 }
