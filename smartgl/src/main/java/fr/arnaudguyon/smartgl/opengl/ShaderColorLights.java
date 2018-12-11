38df811665f62f4970aa46f385f2d492cc827066
Arnaud Guyon <nonuguyon@gmail.com>
2018-12-11 19:22:48 +0100

    Shaders: renaming some attributes, and improved ShaderTextureLights

--- smartgl/src/main/java/fr/arnaudguyon/smartgl/opengl/ShaderColorLights.java
+++ smartgl/src/main/java/fr/arnaudguyon/smartgl/opengl/ShaderColorLights.java
@@ -11,8 +11,8 @@ public class ShaderColorLights extends Shader {
     private final static String VERTEX_SHADER =
             "uniform mat4 m_ProjectionMatrix;" +
                     "uniform mat4 mModelMatrix;" +
-                    "uniform vec3 mLightDirection;" +
-                    "uniform vec4 mLightColor;" +
+                    "uniform vec3 mParallelLightDirection;" +
+                    "uniform vec4 mParallelLightColor;" +
                     "uniform vec4 mAmbiantColor;" +
                     "attribute vec4 m_Position;" +
                     "attribute vec3 mNormals;" +
@@ -22,8 +22,8 @@ public class ShaderColorLights extends Shader {
                     "void main() {" +
                     "  gl_Position = m_ProjectionMatrix * m_Position;" +
                     "  vec3 modelViewNormal = vec3(mModelMatrix * vec4(mNormals, 0.0));" +
-                    "  float diffuse = max(dot(modelViewNormal, -mLightDirection), 0.0);" +
-                    "  vLightColor = mAmbiantColor + (mLightColor * diffuse);" +
+                    "  float intensity = max(dot(modelViewNormal, -mParallelLightDirection), 0.0);" +
+                    "  vLightColor = mAmbiantColor + (mParallelLightColor * intensity);" +
                     "  vColor = m_Color;\n" +
                     "}";
 
@@ -37,8 +37,8 @@ public class ShaderColorLights extends Shader {
                     "}";
 
     private int mModelMatrixId;
-    private int mLightDirectionId;
-    private int mLightColorId;
+    private int mParallelLightDirectionId;
+    private int mParallelLightColorId;
     private int mLightAmbiantId;
     private int mNormalsId;
 
@@ -81,13 +81,13 @@ public class ShaderColorLights extends Shader {
         super.init(programId);
 
         mModelMatrixId = GLES20.glGetUniformLocation(programId, "mModelMatrix");
-        mLightDirectionId = GLES20.glGetUniformLocation(programId, "mLightDirection");
-        mLightColorId = GLES20.glGetUniformLocation(programId, "mLightColor");
+        mParallelLightDirectionId = GLES20.glGetUniformLocation(programId, "mParallelLightDirection");
+        mParallelLightColorId = GLES20.glGetUniformLocation(programId, "mParallelLightColor");
         mLightAmbiantId = GLES20.glGetUniformLocation(programId, "mAmbiantColor");
         mNormalsId = GLES20.glGetAttribLocation(programId, "mNormals");
         Assert.assertTrue(mModelMatrixId >= 0);
-        Assert.assertTrue(mLightDirectionId >= 0);
-        Assert.assertTrue(mLightColorId >= 0);
+        Assert.assertTrue(mParallelLightDirectionId >= 0);
+        Assert.assertTrue(mParallelLightColorId >= 0);
         Assert.assertTrue(mLightAmbiantId >= 0);
         Assert.assertTrue(mNormalsId >= 0);
     }
@@ -99,10 +99,10 @@ public class ShaderColorLights extends Shader {
         GLES20.glUniformMatrix4fv(mModelMatrixId, 1, false, modelMatrix, 0);
 
         float[] lightDirection = renderer.getLightDirection();
-        GLES20.glUniform3fv(mLightDirectionId, 1, lightDirection, 0);
+        GLES20.glUniform3fv(mParallelLightDirectionId, 1, lightDirection, 0);
 
         float[] lightColor = renderer.getLightColor();
-        GLES20.glUniform4fv(mLightColorId, 1, lightColor, 0);
+        GLES20.glUniform4fv(mParallelLightColorId, 1, lightColor, 0);
 
         float[] ambiant = renderer.getLightAmbiant();
         GLES20.glUniform4fv(mLightAmbiantId, 1, ambiant, 0);
